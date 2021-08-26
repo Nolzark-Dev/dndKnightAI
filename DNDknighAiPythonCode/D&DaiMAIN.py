fe4a4e74bc5d65a3613318dcec07f6f804e611e3
Nolzark <89377971+WalterWhiteButAProgrammer@users.noreply.github.com>
2021-08-26 11:26:42 +1000

    Delete D&DaiMAIN.py

--- DNDknighAiPythonCode/D&DaiMAIN.py
+++
@@ -1,5 +0,0 @@
-dicegit = int(input("Insert Dice Num (1,20): "))
-MNSTERHP = int(input("Insert Enemy health: "))
-MNSTERAC = int(input("Insert Enemy AC: "))
-MNSTERATK = int(input("Insert Enemy Attack score: "))
-
